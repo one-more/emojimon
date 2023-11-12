8520a8206929f6265609cc9166c985846fd0cd87
Ori Pomerantz <qbzzt1@gmail.com>
2023-11-12 13:38:52 -0600

    MUD 2.0 next.14 emojimon: starting version

--- packages/contracts/script/PostDeploy.s.sol
+++ packages/contracts/script/PostDeploy.s.sol
@@ -3,12 +3,10 @@ pragma solidity >=0.8.21;
 
 import { Script } from "forge-std/Script.sol";
 import { console } from "forge-std/console.sol";
-import { IWorld } from "../src/codegen/world/IWorld.sol";
 
 contract PostDeploy is Script {
   function run(address worldAddress) external {
     console.log("Deployed world: ", worldAddress);
-    IWorld world = IWorld(worldAddress);
 
     uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
     vm.startBroadcast(deployerPrivateKey);

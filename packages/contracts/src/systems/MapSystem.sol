08be68c0ff38e7a3dd55d56c968d61a4f18061b1
Ori Pomerantz <qbzzt1@gmail.com>
2023-11-11 21:56:24 -0600

    MUD 2.0 next.14 emojimon starting version

--- packages/contracts/src/systems/MapSystem.sol
+++ packages/contracts/src/systems/MapSystem.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.8.21;
 import { System } from "@latticexyz/world/src/System.sol";
 
 contract MapSystem is System {
-  function distance2(uint32 fromX, uint32 fromY, uint32 toX, uint32 toY) internal pure returns (uint32) {
+  function distance2(uint32 deltaX, uint32 deltaY) internal pure returns (uint32) {
     return deltaX * deltaX + deltaY * deltaY;
   }
 }
